ba2d3c08d122d5f6eea3ce8fcd5bfea092a08882
Juan Ferreira Carlos <juanfcarlos.93@gmail.com>
2018-09-21 19:44:23 -0300

    FactoryMethod
    Inserida as Interfaces e seus respectivos objetos implementados

---
+++ src/model/cargo/CargoAnalista.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.cargo;
+
+import interfaces.Cargo;
+
+/**
+ *
+ * @author alunoces
+ */
+public class CargoAnalista implements Cargo {
+
+    @Override
+    public void exibirInfo() {
+        System.out.println("Cargo: Analista de Sistemas");
+    }
+    
+}
---
+++ src/model/fabrica/FabricaSuporte.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.fabrica;
+
+import interfaces.Cargo;
+import interfaces.FabricaCargos;
+import model.cargo.CargoSuporte;
+
+/**
+ *
+ * @author alunoces
+ */
+public class FabricaSuporte implements FabricaCargos {
+
+    @Override
+    public Cargo criarCargo() {
+        return new CargoSuporte();
+    }     
+    
+}
